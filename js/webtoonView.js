c1f8b9407c92a61e707bbb6a7d65e7aa5a9ad7a6
keunha <jkhsmj@naver.com>
2023-07-25 18:13:48 +0900

    webtoon 페이지 호출 오류 수정

--- js/webtoonView.js
+++ js/webtoonView.js
@@ -1,4 +1,4 @@
-import { WebSite, getImageList, SeverUrl } from "./server.js";
+import { getImageList, SeverUrl } from "./server.js";
 
 const divImageContainer = document.querySelector("div.image-gallery");
 const divLocation = document.getElementById("locationCheck");
@@ -7,9 +7,6 @@ const webToonid = urlParams.get("webtoonId");
 let pageNo = urlParams.get("pageNo");
 const maxPageNo = urlParams.get("maxPageNo");
 let isLoading = false;
-
-const webtoonLink = document.getElementById("otherToonWebsite");
-webtoonLink.setAttribute("href", WebSite);
 
 async function loadImageList() {
   const list = await getImageList(webToonid, pageNo).then((html) =>
